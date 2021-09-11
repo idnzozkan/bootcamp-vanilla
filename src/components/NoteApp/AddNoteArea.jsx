fcd66537fed25035152720b3a21a7ca4d2962a5e
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2021-09-11 15:36:10 +0300

    Add creating a new note component with it's functionality

---
+++ src/components/NoteApp/AddNoteArea.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+const AddNoteArea = ({ writtenNotes, setWrittenNotes }) => {
+    const [value, setValue] = useState('')
+
+    const handleChange = (e) => {
+        setValue(e.target.value)
+    }
+
+    const handleCreateNote = () => {
+        setWrittenNotes([...writtenNotes, value])
+        setValue('')
+    }
+
+    return (
+        <div>
+            <textarea value={value} onChange={handleChange} cols="30" rows="10" />
+            <button onClick={handleCreateNote}>Create</button>
+        </div>
+    )
+}
+
+export default AddNoteArea
